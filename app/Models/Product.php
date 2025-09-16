6770a68e9854bd770e96398343eed5a4aed6b3d8
brucie2004 <abdtheking3@gmail.com>
2025-09-16 18:01:04 +0300

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'image',
+        'category_id',
+        'stock_quantity'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function cartItems(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+}
